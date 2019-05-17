adb2a0aa92784a026f0aaf81bf7d7047c46af94a
noman <nomanurrahman@gmail.com>
2019-05-18 01:08:39 +0600

    middleware updated

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('admin/users', 'AdminUsersController');
+Route::group(['middleware' => ['auth', 'role']], function() {
+	Route::resource('admin/users', 'AdminUsersController');
+});
