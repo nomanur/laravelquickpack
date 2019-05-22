e060b13971736f17d19874f3a5421f80fd20e0a6
noman <nomanurrahman@gmail.com>
2019-05-22 22:16:11 +0600

    before sending email

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,1 +1,41 @@
-it is from home
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>home</title>
+		<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+		<link rel="stylesheet" type="text/css" href="{{asset('css/libs.css')}}">
+	</head>
+	<body>
+		
+		<div class="section-padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<nav class="navbar navbar-expand-lg navbar-light bg-light">
+							<a class="navbar-brand" href="#">Navbar</a>
+							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+							</button>
+							<div class="collapse navbar-collapse" id="navbarNav">
+								<ul class="navbar-nav">
+									<li class="nav-item active">
+										<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#">Blog</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#">About</a>
+									</li>
+									
+								</ul>
+							</div>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+		<script type="text/javascript" src="{{asset('js/libs.js')}}"></script>
+	</body>
+</html>
